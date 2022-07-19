92c37c0f4f7e8b1b3816dfa8ef9a604750307474
Yevhen Bondarenko <zekbond@gmail.com>
2022-07-19 19:05:58 +0300

    Add poetry build pipeline

---
+++ housekeeping/build_poetry_package.py
@@ -0,0 +1,23 @@
+"""
+Builds a Poetry package from the current project.
+"""
+
+from pathlib import Path
+
+from python_ci_utilities.console import initialize_ci_console
+from python_ci_utilities.shell import run_shell_command
+from python_ci_utilities.pip import ensure_package_installed
+
+
+def cli():
+    initialize_ci_console()
+
+    project_root = Path(__file__).parent.parent
+
+    ensure_package_installed("poetry")
+
+    run_shell_command("poetry build", cwd=project_root, use_wsl_on_windows=False)
+
+
+if __name__ == '__main__':
+    cli()
