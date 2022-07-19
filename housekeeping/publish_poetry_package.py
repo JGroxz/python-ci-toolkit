d2b97206c911f08423936ac779781809559c89b2
Yevhen Bondarenko <zekbond@gmail.com>
2022-07-20 01:00:32 +0300

    WIP on PyPI authentication script

--- housekeeping/publish_poetry_package.py
+++ housekeeping/publish_poetry_package.py
@@ -32,7 +32,7 @@ def authenticate() -> None:
 
     # Poetry
     run_shell_command(f"poetry config http-basic.{ENV_AWS_PYPI_REPO_NAME} {pypi_user} {pypi_token}", cwd=project_root, use_wsl_on_windows=False)
-    run_shell_command(f"poetry config repositories.{ENV_AWS_PYPI_REPO_NAME} https://{pypi_user}:{pypi_token}@{pypi_url}", cwd=project_root, use_wsl_on_windows=False)
+    run_shell_command(f"poetry config repositories.{ENV_AWS_PYPI_REPO_NAME} https://{pypi_user}:{pypi_token}@{pypi_url_simple}", cwd=project_root, use_wsl_on_windows=False)
 
 
 def cli():
