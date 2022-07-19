d354e0d22dc4920e6cc7de0d4ff25bdf334f5c2d
Yevhen Bondarenko <zekbond@gmail.com>
2022-07-19 19:19:41 +0300

    Update README

--- README.md
+++ README.md
@@ -5,3 +5,5 @@ Collection of Python scripts useful in our CI/CD pipelines.
 ```bash
 pip install python-ci-utilities
 ```
+
+This package is preinstalled in the [default Python CI runner image](https://eu-west-1.console.aws.amazon.com/ecr/repositories/private/755432789552/pyci/runner?region=eu-west-1).
--- housekeeping/build_poetry_package.py
+++ housekeeping/build_poetry_package.py
@@ -1,7 +1,7 @@
 """
 Builds a Poetry package from the current project.
 """
-
+import logging
 from pathlib import Path
 
 from python_ci_utilities.console import initialize_ci_console
@@ -16,7 +16,11 @@ def cli():
 
     ensure_package_installed("poetry")
 
+    logging.info("Starting Poetry build...")
+
     run_shell_command("poetry build", cwd=project_root, use_wsl_on_windows=False)
+
+    logging.info("Poetry build completed.")
 
 
 if __name__ == '__main__':
