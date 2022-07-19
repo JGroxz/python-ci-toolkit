572a20afa9297a25307ecc6b897135c100b5232d
Yevhen Bondarenko <zekbond@gmail.com>
2022-07-20 00:34:53 +0300

    WIP on PyPI authentication script

--- housekeeping/authenticate_pypi.py
+++ housekeeping/authenticate_pypi.py
@@ -27,15 +27,14 @@ def cli():
     )
     pypi_token = output.strip()
     pypi_user = "aws"
-    pypi_url = f"{ENV_AWS_DOMAIN}-{ENV_AWS_DOMAIN_OWNER}.d.codeartifact.{ENV_AWS_DEFAULT_REGION}.amazonaws.com/pypi/{ENV_AWS_PYPI_REPO_NAME}/"
-    pypi_url_simple = f"{pypi_url}simple/"
+    pypi_url = f"{ENV_AWS_DOMAIN}-{ENV_AWS_DOMAIN_OWNER}.d.codeartifact.{ENV_AWS_DEFAULT_REGION}.amazonaws.com/pypi/{ENV_AWS_PYPI_REPO_NAME}/simple/"
 
     # PIP
-    run_shell_command(f"pip config set global.extra-index-url https://{pypi_user}:{pypi_token}@{pypi_url_simple}", cwd=project_root, use_wsl_on_windows=False)
+    run_shell_command(f"pip config set global.extra-index-url https://{pypi_user}:{pypi_token}@{pypi_url}", cwd=project_root, use_wsl_on_windows=False)
 
     # Poetry
     run_shell_command(f"poetry config http-basic.{ENV_AWS_PYPI_REPO_NAME} {pypi_user} {pypi_token}", cwd=project_root, use_wsl_on_windows=False)
-    run_shell_command(f"poetry config repositories.{ENV_AWS_PYPI_REPO_NAME} https://{pypi_url}")
+    run_shell_command(f"poetry config repositories.{ENV_AWS_PYPI_REPO_NAME} https://{pypi_url}", cwd=project_root, use_wsl_on_windows=False)
 
 
 if __name__ == '__main__':
