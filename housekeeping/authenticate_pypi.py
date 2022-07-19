cb9f571026f3d8e3da3d988b043272a049c0d642
Yevhen Bondarenko <zekbond@gmail.com>
2022-07-20 00:07:15 +0300

    Update PyPI authentication script

--- housekeeping/authenticate_pypi.py
+++ housekeeping/authenticate_pypi.py
@@ -1,6 +1,7 @@
 """
 Adds custom registry to PIP and Poetry configurations.
 """
+from pathlib import Path
 
 from python_ci_utilities.console import initialize_ci_console
 from python_ci_utilities.environment import assert_environment_variable_set
@@ -18,6 +19,8 @@ def cli():
 
     ensure_package_installed("poetry")
 
+    project_root = Path(__file__).parent.parent
+
     _, output = run_shell_command(
         command=f"aws codeartifact get-authorization-token --domain {ENV_AWS_DOMAIN} --domain-owner {ENV_AWS_DOMAIN_OWNER} --query authorizationToken --duration-seconds 3600 --output text",
         silence_output=True
@@ -27,10 +30,10 @@ def cli():
     pypi_url = f"{ENV_AWS_DOMAIN}-{ENV_AWS_DOMAIN_OWNER}.d.codeartifact.{ENV_AWS_DEFAULT_REGION}.amazonaws.com/pypi/{ENV_AWS_PYPI_REPO_NAME}/simple/"
 
     # PIP
-    run_shell_command(f"pip config set global.extra-index-url https://{pypi_user}:{pypi_token}@{pypi_url}", use_wsl_on_windows=False)
+    run_shell_command(f"pip config set global.extra-index-url https://{pypi_user}:{pypi_token}@{pypi_url}", cwd=project_root, use_wsl_on_windows=False)
 
     # Poetry
-    run_shell_command(f"poetry config http-basic.{ENV_AWS_PYPI_REPO_NAME} {pypi_user} {pypi_token}", use_wsl_on_windows=False)
+    run_shell_command(f"poetry config http-basic.{ENV_AWS_PYPI_REPO_NAME} {pypi_user} {pypi_token}", cwd=project_root, use_wsl_on_windows=False)
 
 
 if __name__ == '__main__':
