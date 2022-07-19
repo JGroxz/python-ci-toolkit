6a2e198ad2d1d5ebb6cb78c704567c81a8c675c4
Yevhen Bondarenko <zekbond@gmail.com>
2022-07-19 23:57:31 +0300

    Add PYPI authentication step to the pipeline

---
+++ housekeeping/authenticate_pypi.py
@@ -0,0 +1,37 @@
+"""
+Adds custom registry to PIP and Poetry configurations.
+"""
+
+from python_ci_utilities.console import initialize_ci_console
+from python_ci_utilities.environment import assert_environment_variable_set
+from python_ci_utilities.pip import ensure_package_installed
+from python_ci_utilities.shell import run_shell_command
+
+ENV_AWS_DOMAIN = assert_environment_variable_set("AWS_DOMAIN")
+ENV_AWS_DOMAIN_OWNER = assert_environment_variable_set("AWS_DOMAIN_OWNER")
+ENV_AWS_DEFAULT_REGION = assert_environment_variable_set("AWS_DEFAULT_REGION")
+ENV_AWS_PYPI_REPO_NAME = assert_environment_variable_set("AWS_PYPI_REPO_NAME")
+
+
+def cli():
+    initialize_ci_console()
+
+    ensure_package_installed("poetry")
+
+    _, output = run_shell_command(
+        command=f"aws codeartifact get-authorization-token --domain {ENV_AWS_DOMAIN} --domain-owner {ENV_AWS_DOMAIN_OWNER} --query authorizationToken --duration-seconds 3600 --output text",
+        silence_output=True
+    )
+    pypi_token = output.strip()
+    pypi_user = "aws"
+    pypi_url = f"{ENV_AWS_DOMAIN}-{ENV_AWS_DOMAIN_OWNER}.d.codeartifact.{ENV_AWS_DEFAULT_REGION}.amazonaws.com/pypi/{ENV_AWS_PYPI_REPO_NAME}/simple/"
+
+    # PIP
+    run_shell_command(f"pip config set global.extra-index-url https://{pypi_user}:{pypi_token}@{pypi_url}", use_wsl_on_windows=False)
+
+    # Poetry
+    run_shell_command(f"poetry config http-basic.{ENV_AWS_PYPI_REPO_NAME} {pypi_user} {pypi_token}", use_wsl_on_windows=False)
+
+
+if __name__ == '__main__':
+    cli()
