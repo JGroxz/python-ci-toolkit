3904927ef3e9896ecadc4cad25fbb2050bfe2a38
Yevhen Bondarenko <zekbond@gmail.com>
2022-07-22 21:55:34 +0300

    Update CI scripts

--- housekeeping/build_poetry_package.py
+++ housekeeping/build_poetry_package.py
@@ -2,23 +2,21 @@
 Builds a Poetry package from the current project.
 """
 import logging
-from pathlib import Path
 
 from python_ci_toolkit.console import initialize_ci_console
-from python_ci_toolkit.shell import run_shell_command
+from python_ci_toolkit.environment import ci_project_root
 from python_ci_toolkit.pip import ensure_package_installed
+from python_ci_toolkit.shell import run_shell_command
 
 
 def cli():
     initialize_ci_console()
 
-    project_root = Path(__file__).parent.parent
-
     ensure_package_installed("poetry")
 
     logging.info("Starting Poetry build...")
 
-    run_shell_command("poetry build", cwd=project_root, use_wsl_on_windows=False)
+    run_shell_command("poetry build", cwd=ci_project_root, use_wsl_on_windows=False)
 
     logging.info("Poetry build completed.")
 
--- housekeeping/publish_poetry_package.py
+++ housekeeping/publish_poetry_package.py
@@ -7,7 +7,7 @@ from pathlib import Path
 import toml
 
 from python_ci_toolkit.console import initialize_ci_console
-from python_ci_toolkit.environment import assert_environment_variable_set
+from python_ci_toolkit.environment import assert_environment_variable_set, ci_project_root
 from python_ci_toolkit.pip import ensure_package_installed
 from python_ci_toolkit.shell import run_shell_command
 from python_ci_toolkit.versions import get_latest_pypi_package_version, get_project_version_from_file
@@ -21,8 +21,6 @@ ENV_AWS_PYPI_REPO_NAME = assert_environment_variable_set("AWS_PYPI_REPO_NAME")
 def authenticate() -> None:
     logging.info(f"Authenticating to PyPI repository '{ENV_AWS_PYPI_REPO_NAME}'...")
 
-    project_root = Path(__file__).parent.parent
-
     _, output = run_shell_command(
         command=f"aws codeartifact get-authorization-token --domain {ENV_AWS_DOMAIN} --domain-owner {ENV_AWS_DOMAIN_OWNER} --query authorizationToken --duration-seconds 3600 --output text",
         silence_output=True
@@ -33,43 +31,40 @@ def authenticate() -> None:
     pypi_url_simple = f"{pypi_url}simple/"
 
     # PIP
-    run_shell_command(f"pip config set global.extra-index-url https://{pypi_user}:{pypi_token}@{pypi_url_simple}", cwd=project_root, use_wsl_on_windows=False)
+    run_shell_command(f"pip config set global.extra-index-url https://{pypi_user}:{pypi_token}@{pypi_url_simple}", cwd=ci_project_root, use_wsl_on_windows=False)
 
     # Poetry
-    run_shell_command(f"poetry config http-basic.{ENV_AWS_PYPI_REPO_NAME} {pypi_user} {pypi_token}", cwd=project_root, use_wsl_on_windows=False)
-    run_shell_command(f"poetry config repositories.{ENV_AWS_PYPI_REPO_NAME} https://{pypi_url}", cwd=project_root, use_wsl_on_windows=False)
+    run_shell_command(f"poetry config http-basic.{ENV_AWS_PYPI_REPO_NAME} {pypi_user} {pypi_token}", cwd=ci_project_root, use_wsl_on_windows=False)
+    run_shell_command(f"poetry config repositories.{ENV_AWS_PYPI_REPO_NAME} https://{pypi_url}", cwd=ci_project_root, use_wsl_on_windows=False)
 
 
 def publish() -> None:
     logging.info(f"Publishing package to the PYPI repository '{ENV_AWS_PYPI_REPO_NAME}'...")
 
-    project_root = Path(__file__).parent.parent
-
     package_name = get_poetry_project_package_name()
 
     latest_version = get_latest_pypi_package_version(package_name)
-    local_version = get_project_version_from_file(project_root)
+    local_version = get_project_version_from_file(ci_project_root)
 
     # check if we need to publish by comparing our local version to the latest one in the remote repo
     if latest_version == local_version:
         logging.info(f"Version '{latest_version}' of '{package_name}' already exists in the target repository '{ENV_AWS_PYPI_REPO_NAME}'. Will not publish.")
         return
     if latest_version > local_version:
-        logging.warning(f"Local version of the package '{package_name}' ('{local_version}') is lower than the latest version in the target repository '{ENV_AWS_PYPI_REPO_NAME}' ('{latest_version}').\n"
-                        f"    Something could be wrong. Please check the CI logic.")
+        logging.warning(
+            f"Local version of the package '{package_name}' ('{local_version}') is lower than the latest version in the target repository '{ENV_AWS_PYPI_REPO_NAME}' ('{latest_version}').\n"
+            f"    Something could be wrong. Please check the CI logic.")
         exit(1)
 
-    run_shell_command(f"poetry publish --repository {ENV_AWS_PYPI_REPO_NAME}", cwd=project_root, use_wsl_on_windows=False)
+    run_shell_command(f"poetry publish --repository {ENV_AWS_PYPI_REPO_NAME}", cwd=ci_project_root, use_wsl_on_windows=False)
 
     logging.info(f"Package '{package_name}' version '{local_version}' successfully published to '{ENV_AWS_PYPI_REPO_NAME}' repository.")
 
 
 def get_poetry_project_package_name() -> str:
-    project_root = Path(__file__).parent.parent
-
-    pyproject_toml_path = Path(project_root, "pyproject.toml")
+    pyproject_toml_path = Path(ci_project_root, "pyproject.toml")
     if not pyproject_toml_path.exists():
-        logging.error(f"'pyproject.toml' was not found in the project root folder ('{project_root}').\n"
+        logging.error(f"'pyproject.toml' was not found in the project root folder ('{ci_project_root}').\n"
                       f"    Make sure you are trying to publish a poetry project.")
         exit(1)
 
