15fcc9b8a19ac802e65c8f1b50782ac6edc6796b
Yevhen Bondarenko <zekbond@gmail.com>
2022-10-12 01:27:41 +0300

    Make run_shell_command() lazy-load ci_console

--- python_ci_toolkit/shell.py
+++ python_ci_toolkit/shell.py
@@ -13,17 +13,19 @@ from rich.style import Style
 from rich.table import Table
 from rich.text import Text
 
-from .console import ci_console
-
 SHELL_OUTPUT_PREFIX_WIDTH_MIN = 15
 SHELL_OUTPUT_PREFIX_WIDTH_MAX = 26
 SHELL_OUTPUT_PREFIX_STYLE = Style(color="blue")
 SHELL_OUTPUT_COMMAND_STYLE = Style(color="deep_sky_blue4", italic=True)
 
 
+ci_console = None
+
+
 def run_shell_command(command: str,
                       cwd: str | Path = os.getcwd(),
                       silence_output: bool = False,
+                      raw_output: bool = False,
                       throw_exception_on_error: bool = True,
                       use_wsl_on_windows: bool = True) -> Tuple[int, str | None]:
     """
@@ -36,6 +38,9 @@ def run_shell_command(command: str,
         command: Command to execute.
         cwd: Working directory to execute the command in. Defaults to current working directory.
         silence_output: If set to True, command output will be suppressed.
+        raw_output: If set to True, the output from the executed command will be printed as is.
+            If set to False, the output will be printed with pretty Rich formatting through ci_console.
+            Has effect only with 'silence_output' set to False.
         throw_exception_on_error: If set to True (default), an exception will be thrown if the executed command exits with a non-zero exit code.
         use_wsl_on_windows: If set to True (default) and running on Windows, the provided command will be run in WSL.
 
@@ -46,6 +51,12 @@ def run_shell_command(command: str,
         RuntimeError:
             if the executed command completes with a non-zero exit code.
     """
+
+    # lazy-initialize CI console if pretty output is requested
+    global ci_console
+    if (not silence_output) and (not raw_output) and (ci_console is None):
+        from .console import ci_console as c
+        ci_console = c
 
     if os.name == "nt" and use_wsl_on_windows:
         # use WSL on Windows
@@ -65,15 +76,19 @@ def run_shell_command(command: str,
 
             # print to console if not silenced
             if not silence_output:
-                grid = Table.grid()
-                grid.add_column(style=SHELL_OUTPUT_PREFIX_STYLE, min_width=SHELL_OUTPUT_PREFIX_WIDTH_MIN, max_width=SHELL_OUTPUT_PREFIX_WIDTH_MAX, overflow="ellipsis", no_wrap=True)
-                grid.add_column(style=SHELL_OUTPUT_PREFIX_STYLE)
-                grid.add_column(overflow="fold")
-                grid.add_row(
-                    Text(f" > shell: ") + Text(command, style=SHELL_OUTPUT_COMMAND_STYLE), " │ ", decoded_line.rstrip(" \n")
-                )
+                decoded_line = decoded_line.rstrip(" \n")
+                if raw_output:
+                    print(decoded_line)
+                else:
+                    grid = Table.grid()
+                    grid.add_column(style=SHELL_OUTPUT_PREFIX_STYLE, min_width=SHELL_OUTPUT_PREFIX_WIDTH_MIN, max_width=SHELL_OUTPUT_PREFIX_WIDTH_MAX, overflow="ellipsis", no_wrap=True)
+                    grid.add_column(style=SHELL_OUTPUT_PREFIX_STYLE)
+                    grid.add_column(overflow="fold")
+                    grid.add_row(
+                        Text(f" > shell: ") + Text(command, style=SHELL_OUTPUT_COMMAND_STYLE), " │ ", decoded_line
+                    )
 
-                ci_console.print(grid, end="")
+                    ci_console.print(grid, end="")
 
     process = subprocess.Popen(args, cwd=cwd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     with process.stdout:
