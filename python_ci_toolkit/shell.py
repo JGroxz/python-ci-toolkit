a380bdeaf75ee97a56631065b10cd2fd848f1b40
Yevhen Bondarenko <zekbond@gmail.com>
2022-10-13 17:49:43 +0300

    Print header with full executed command when using run_shell_command()

--- python_ci_toolkit/shell.py
+++ python_ci_toolkit/shell.py
@@ -18,7 +18,6 @@ SHELL_OUTPUT_PREFIX_WIDTH_MAX = 26
 SHELL_OUTPUT_PREFIX_STYLE = Style(color="blue")
 SHELL_OUTPUT_COMMAND_STYLE = Style(color="deep_sky_blue4", italic=True)
 
-
 ci_console = None
 
 
@@ -52,20 +51,27 @@ def run_shell_command(command: str,
             if the executed command completes with a non-zero exit code.
     """
 
-    # lazy-initialize CI console if pretty output is requested
+    # lazy-initialize CI console if using pretty output
     global ci_console
     if (not silence_output) and (not raw_output) and (ci_console is None):
         from .console import ci_console as c
         ci_console = c
 
+    # print header if using pretty output
+    if (not silence_output) and (not raw_output):
+        header = Text("Running shell command:", style=SHELL_OUTPUT_PREFIX_STYLE) + " " + Text(f"{command}", style=SHELL_OUTPUT_COMMAND_STYLE)
+        ci_console.print(header)
+
+    # use WSL if required on Windows
     if os.name == "nt" and use_wsl_on_windows:
-        # use WSL on Windows
         command = f"wsl {command}"
 
+    # prepare command args
     args = shlex.split(command)
 
     captured_output = ""
 
+    # helper function for handling the executed shell command's output
     def capture_subprocess_output(pipe):
         for line in iter(pipe.readline, b''):  # b'\n'-separated lines
             decoded_line = line.decode("utf-8")
@@ -90,6 +96,7 @@ def run_shell_command(command: str,
 
                     ci_console.print(grid, end="")
 
+    # run the shell command and capture its output
     process = subprocess.Popen(args, cwd=cwd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     with process.stdout:
         with process.stderr:
