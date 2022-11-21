c5d4c5f2f335e0e7d91d1f93c273dd7761233387
Yevhen Bondarenko <zekbond@gmail.com>
2022-11-21 19:52:48 +0200

    Handle the import of git module gracefully when no Git repo is found in the project

--- python_ci_toolkit/git.py
+++ python_ci_toolkit/git.py
@@ -6,14 +6,18 @@ import logging
 import os
 from pathlib import Path
 
-from git import Repo, GitCommandError
+from git import Repo, GitCommandError, InvalidGitRepositoryError
 
 from python_ci_toolkit.environment import ci_project_root, ci_environment_type, CiEnvironmentType, assert_environment_variable_set, ci_temp_files_directory
 
 TEMP_PRIVATE_SSH_KEY_FILE_PATH = ci_temp_files_directory.joinpath("ssh_key")
 
-ci_repo = Repo(ci_project_root)
-"""GitPython reference to the local Git repository of the current CI project."""
+try:
+    ci_repo = Repo(ci_project_root)
+    """GitPython reference to the local Git repository of the current CI project."""
+except InvalidGitRepositoryError as e:
+    ci_repo = None
+    logging.error(f"Current project path ('{ci_project_root}') is not a Git repository. Module 'python_ci_toolkit.git' module cannot be used.")
 
 
 def get_default_ssh_private_key_file_path() -> Path:
