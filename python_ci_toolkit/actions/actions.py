db8637c574b990af8be27330a03caa418fa75d21
Yevhen Bondarenko <zekbond@gmail.com>
2023-01-22 03:11:18 +0200

    Implement local caching of remote action repositories

--- python_ci_toolkit/actions/actions.py
+++ python_ci_toolkit/actions/actions.py
@@ -5,7 +5,6 @@ import hashlib
 import logging
 import os
 import shutil
-import stat
 import sys
 import time
 from pathlib import Path
@@ -14,33 +13,60 @@ from typing import List
 from python_ci_toolkit import pip
 from ..environment import assert_environment_variable_set, assert_multiline_environment_variable_set, \
     ci_files_directory, ci_temp_files_directory, get_ci_environment_name, ci_project_root
-from ..git import git_ssh_credentials, get_default_ssh_private_key
+from ..git import git_ssh_credentials, get_default_ssh_private_key, delete_git_repo
 from ..python import import_module_from_file
 from ..shell import run_shell_command
 
 # Constants
 ACTION_VERSION_SEPARATOR = "@"
-DOWNLOADED_ACTIONS_DIRECTORY_PATH: Path = ci_temp_files_directory.joinpath("downloaded_actions")
+# DOWNLOADED_ACTIONS_DIRECTORY_PATH = ci_temp_files_directory / "downloaded_actions"
+DEFAULT_ACTION_REPO_URL = "git@bitbucket.org:pyci/python-ci-actions.git"
+DOWNLOADED_ACTION_REPOS_DIRECTORY = ci_temp_files_directory / "downloaded_action_repos"
+
+logger = logging.getLogger(__name__)
 
 
-def delete_git_repo(repo_path: Path) -> None:
+def retrieve_action_repo(git_repo_url: str, ssh_private_key: str = None) -> Path:
     """
-    Deletes Git repository in the given folder.
+    Retrieves the given Git repository into the standard actions cache location.
 
     Notes:
-        Deleting Git directory requires special treatment, because a normal shutil.rmtree() call can fail
-        because of certain files in .git folder which get marked as read-only when cloning.
+        This action will return the existing local repo if it was cloned before, but will fetch all available updates regardless before that.
 
     Args:
-        repo_path: Path to the Git repository's folder.
+        git_repo_url: URL of the Git repository to pull.
+        ssh_private_key: Private SSH key to be used when accessing the specified Git repository (string value).
+
+    Returns:
+        Path to the cloned repository.
     """
 
-    def on_rm_error(func, path, exc_info):
-        # from: https://stackoverflow.com/a/4829285
-        os.chmod(path, stat.S_IWRITE)
-        os.unlink(path)
+    # prepare downloads directory
+    os.makedirs(DOWNLOADED_ACTION_REPOS_DIRECTORY, exist_ok=True)
 
-    shutil.rmtree(repo_path, onerror=on_rm_error)
+    # generate local repo path based on remote
+    git_repo_hash = hashlib.md5(git_repo_url.encode()).hexdigest()
+    cloned_repo_path = DOWNLOADED_ACTION_REPOS_DIRECTORY / git_repo_hash
+
+    with git_ssh_credentials(ssh_private_key):
+        if not cloned_repo_path.exists():
+            logger.debug(f"Cloning action repo from '{git_repo_url}' (md5: {git_repo_hash}).")
+
+            # execute a fresh pull
+            run_shell_command(f'git clone "{git_repo_url}" "{cloned_repo_path}"',
+                              silence_output=True, use_wsl_on_windows=False)
+        else:
+            logger.debug(f"Using cached action repo of '{git_repo_url}' (md5: {git_repo_hash}).")
+
+        # fetch all available remote branches and tags
+        run_shell_command("git fetch --all --tags",
+                          cwd=cloned_repo_path, silence_output=True, use_wsl_on_windows=False)
+
+        # switch existing clone to main branch
+        run_shell_command("git checkout main",
+                          cwd=cloned_repo_path, silence_output=True, use_wsl_on_windows=False)
+
+    return cloned_repo_path
 
 
 def retrieve_ci_action_script_from_git(git_repo_url: str, action_name: str, action_version: str = None,
@@ -65,108 +91,57 @@ def retrieve_ci_action_script_from_git(git_repo_url: str, action_name: str, acti
     Returns:
         Local path to the downloaded action file.
     """
+
+    # retrieve repo
+    action_repo_directory = retrieve_action_repo(git_repo_url, ssh_private_key)
+
+    # prepare paths
     action_display_name = f"{action_name}{ACTION_VERSION_SEPARATOR}{action_version}"
+    action_script_directory = action_repo_directory / "actions" / f"{action_name}"
+    action_script_path = action_script_directory / f"{action_name}.py"
+    action_requirements_path = action_script_directory / "requirements.txt"
 
-    # generate local repo path based on remote
-    action_repos_directory = ci_temp_files_directory.joinpath("downloaded_action_repos")
-    git_repo_hash = hashlib.md5(git_repo_url.encode()).hexdigest()
-    cloned_repo_path = action_repos_directory.joinpath(git_repo_hash)
-
-    # prepare other paths
-    action_script_name = f"{action_name}.py"
-    action_script_cloned_path = Path(cloned_repo_path, "actions", f"{action_name}", f"{action_name}.py")
-    action_local_directory = DOWNLOADED_ACTIONS_DIRECTORY_PATH.joinpath(action_name)
-    action_script_local_path = action_local_directory.joinpath(action_script_name)
-    action_requirements_local_path = action_local_directory.joinpath("requirements.txt")
-
-    # prepare directories
-    os.makedirs(action_local_directory, exist_ok=True)
-    os.makedirs(ci_temp_files_directory, exist_ok=True)
-
-    # TODO: rework cloning logic to enable caching
-    #       - if not cloned, clone the full repo
-    #       - if cloned, fetch all
-    #       - checkout branch/tag from origin
-
-    if cloned_repo_path.exists():
-        logging.warning("REPO ALREADY CLONED")
-        run_shell_command(f'git status',
-                          cwd=cloned_repo_path, silence_output=False, use_wsl_on_windows=False)
-
-        start = time.perf_counter()
-        run_shell_command("git fetch --all --tags")
-        print(f"{time.perf_counter() - start}")
-
-        if action_version:
-            run_shell_command(f'git checkout "origin/tags/{action_version}"',
-                              cwd=cloned_repo_path, silence_output=False, use_wsl_on_windows=False)
-
-        run_shell_command(f'git status',
-                          cwd=cloned_repo_path, silence_output=False, use_wsl_on_windows=False)
-
-        raise NotImplementedError("WIP")
+    # TODO: checkout required branch/tag
+    # TODO: update
 
     with git_ssh_credentials(ssh_private_key):
         # clone the repo
         if action_version is None:
-            # if no version specified, just clone the main branch
-            run_shell_command(f'git clone --depth 1 "{git_repo_url}" "{cloned_repo_path}"',
-                              silence_output=True, use_wsl_on_windows=False)
+            # if no version specified, use the main branch
+            run_shell_command(f'git checkout main',
+                              cwd=action_repo_directory, silence_output=True, use_wsl_on_windows=False)
         else:
             # find branches or tags matching the given version
-            _, output = run_shell_command(f'git ls-remote "{git_repo_url}"',
-                                          silence_output=True, use_wsl_on_windows=False)
-            output_lines = output.split("\n")
-            branch_exists = False
-            tag_exists = False
-            for line in output_lines:
-                if f"refs/heads/{action_version}" in line:
-                    branch_exists = True
-                if f"refs/tags/{action_version}" in line:
-                    tag_exists = True
+            _, output = run_shell_command(f'git show-ref',
+                                          cwd=action_repo_directory, silence_output=True, use_wsl_on_windows=False)
+            branch_exists = (f"refs/heads/{action_version}" in output) or (f"refs/remotes/origin/{action_version}" in output)
+            tag_exists = (f"refs/tags/{action_version}" in output)
 
             # if both a branch and a tag exist with the same name, do not pull to avoid ambiguity
             if tag_exists and branch_exists:
-                logging.error(
-                    f"Both a branch and a tag named '{action_version}' exist in remote repository '{git_repo_url}'.\n"
-                    f"The action '{action_display_name}' wil not be pulled to avoid ambiguity.\n"
-                    f"Please remove the redundant branch or tag ('{action_version}') from the repo before pulling this version again.")
+                logger.error(f"Both a branch and a tag named '{action_version}' exist in remote repository '{git_repo_url}'.\n"
+                             f"The action '{action_display_name}' wil not be pulled to avoid ambiguity.\n"
+                             f"Please remove the redundant branch or tag ('{action_version}') from the repo before pulling this version again.")
                 sys.exit(2)
 
             # if nothing matches the version, there is nothing we can do
             if (not tag_exists) and (not branch_exists):
-                logging.error(f"Cannot pull action '{action_display_name}' from Git: "
-                              f"remote repository '{git_repo_url}' has neither a branch nor a tag named '{action_version}'.\n"
-                              f"Please make sure that the corresponding branch or tag ('{action_version}') exists before pulling this version again.")
+                logger.error(f"Cannot pull action '{action_display_name}' from Git: "
+                             f"Repository '{git_repo_url}' has neither a branch nor a tag named '{action_version}'.\n"
+                             f"Please make sure that the corresponding branch or tag ('{action_version}') exists before pulling this version again.")
                 sys.exit(3)
 
-            # clear the way for the new clone
-            if cloned_repo_path.exists():
-                delete_git_repo(cloned_repo_path)
-
             # pull whatever is available
-            run_shell_command(f'git clone --branch "{action_version}" "{git_repo_url}" "{cloned_repo_path}"',
-                              silence_output=True, use_wsl_on_windows=False)
+            run_shell_command(f'git checkout "{action_version}"',
+                              cwd=action_repo_directory, silence_output=True, use_wsl_on_windows=False)
 
     # check if the repo had the requested action script
-    if not action_script_cloned_path.exists():
-        logging.error(
-            f"Cloned repository '{git_repo_url}' does include action '{action_name}' (expected script path is '{action_script_cloned_path}').\n"
-            f"Please make sure that the remote repository has the required action script.")
+    if not action_script_path.exists():
+        logger.error(f"Cloned repository '{git_repo_url}' does include action '{action_name}' (expected script path is '{action_script_path}').\n"
+                     f"Please make sure that the remote repository has the required action script.")
         sys.exit(4)
 
-    # copy the action script over to the downloaded actions directory
-    shutil.copyfile(action_script_cloned_path, action_script_local_path)
-
-    # copy requirements if those are present
-    action_requirements_cloned_path = action_script_cloned_path.parent.joinpath("requirements.txt")
-    if action_requirements_cloned_path.exists():
-        shutil.copyfile(action_requirements_cloned_path, action_requirements_local_path)
-
-    # # clean up
-    # delete_git_repo(cloned_repo_path)
-
-    return action_script_local_path
+    return action_script_path
 
 
 def retrieve_ci_action_script_local(action_name: str) -> Path:
@@ -203,11 +178,11 @@ def _print_action_header(action_name: str, action_version: str, action_source: s
 
     action_display_name = f"{action_name}{ACTION_VERSION_SEPARATOR}{action_version}";
 
-    logging.info(f"[green]>_[/][rgb(146,202,85)] Running CI action '{action_display_name}'[/]...\n"
-                 f"     CI toolkit version: [blue]{version}[/]\n"
-                 f"     CI environment: [blue]{ci_environment_name}[/]\n"
-                 f"     Action version: [blue]{action_version}[/]\n"
-                 f"     Action source: {action_source}", extra={"markup": True, "highlighter": None})
+    logger.info(f"[green]>_[/][rgb(146,202,85)] Running CI action '{action_display_name}'[/]...\n"
+                f"     CI toolkit version: [blue]{version}[/]\n"
+                f"     CI environment: [blue]{ci_environment_name}[/]\n"
+                f"     Action version: [blue]{action_version}[/]\n"
+                f"     Action source: {action_source}", extra={"markup": True, "highlighter": None})
 
 
 def run_ci_action(action_name: str, action_version: str = None, argv: List[str] = None) -> None:
@@ -219,12 +194,12 @@ def run_ci_action(action_name: str, action_version: str = None, argv: List[str] 
                            else f"{action_name}{ACTION_VERSION_SEPARATOR}{action_version}")
 
     # locate action script
-    logging.debug(f"Locating action '{action_display_name}'...")
+    logger.debug(f"Locating action '{action_display_name}'...")
     if action_version == "local":
         # local folder
         action_script_path = retrieve_ci_action_script_local(action_name)
         action_source = f"'{action_script_path}'"
-        logging.debug(f"Retrieved local action '{action_name}'. Running...")
+        logger.debug(f"Retrieved local action '{action_name}'. Running...")
     else:
         # Git repo
         start_time = time.perf_counter()
@@ -232,7 +207,7 @@ def run_ci_action(action_name: str, action_version: str = None, argv: List[str] 
         actions_git_repo_url = assert_environment_variable_set(
             "PYTHON_CI_ACTIONS_GIT_REPO_URL",
             f"URL address of the Git repository is required to pull the code for action '{action_display_name}'.",
-            fallback_value_getter=lambda: "git@bitbucket.org:pyci/python-ci-actions.git")
+            fallback_value_getter=lambda: DEFAULT_ACTION_REPO_URL)
         actions_ssh_private_key = assert_multiline_environment_variable_set(
             "PYTHON_CI_ACTIONS_SSH_PRIVATE_KEY",
             "SSH private key is required to pull actions from the private remote Git repositories.",
@@ -249,14 +224,14 @@ def run_ci_action(action_name: str, action_version: str = None, argv: List[str] 
         action_source = f"'{branch_or_tag_name}' at '{actions_git_repo_url}'"
 
         duration = time.perf_counter() - start_time
-        logging.info(f"Retrieved action '{action_display_name}' from Git in {duration:.3f} seconds.")
+        logger.info(f"Retrieved action '{action_display_name}' from Git in {duration:.3f} seconds.")
 
     # install action's requirements if present
     action_requirements_path = action_script_path.parent.joinpath("requirements.txt")
     if action_requirements_path.exists():
-        logging.debug(f"Action '{action_name}' has requirements file supplied with it. Installing requirements...")
+        logger.debug(f"Action '{action_name}' has requirements file supplied with it. Installing requirements...")
         pip.ensure_requirements_installed(action_requirements_path)
-        logging.debug("Requirements installation complete.")
+        logger.debug("Requirements installation complete.")
 
     # prepare action's CLI arguments
     sys.argv = sys.argv[:1]
@@ -265,11 +240,11 @@ def run_ci_action(action_name: str, action_version: str = None, argv: List[str] 
 
     # run CI action using its cli() method with the given arguments
     try:
-        logging.debug(f"Importing Python module of the action '{action_name}'...")
+        logger.debug(f"Importing Python module of the action '{action_name}'...")
         action_module = import_module_from_file(f"{action_name}", f"{action_script_path}")
-        logging.debug("Import completed.")
+        logger.debug("Import completed.")
     except Exception:
-        logging.error(
+        logger.error(
             f"Error when importing Python module from action script '{action_script_path}' (action '{action_display_name}' from {action_source}).")
         raise
 
