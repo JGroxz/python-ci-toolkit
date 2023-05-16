af704ac3844b75f441c6aa4f8f332deed82fb1c1
Yevhen Bondarenko <zekbond@gmail.com>
2023-05-16 17:52:16 +0300

    Silence shell command fixing git workspace ownership

--- python_ci_toolkit/environment/__init__.py
+++ python_ci_toolkit/environment/__init__.py
@@ -6,4 +6,4 @@ if ci_environment_type == CiEnvironmentType.GitHubActions:
     # GitHub Actions workspace directory belongs to a different user out-of-the-box,
     # so we have to mark it as safe to be able to run all git commands without errors.
     # See for more info: https://github.com/python-semantic-release/python-semantic-release/issues/560
-    run_shell_command(f'git config --global --add safe.directory "{ci_project_root}"')
+    run_shell_command(f'git config --global --add safe.directory "{ci_project_root}"', silence_output=True, use_wsl_on_windows=False)
