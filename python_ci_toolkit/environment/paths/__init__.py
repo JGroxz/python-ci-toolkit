5787f4d04020b8318d4a95ae2f1d6ebd244a8a9f
Yevhen Bondarenko <zekbond@gmail.com>
2024-01-25 15:36:27 +0200

    Rearrange the innards of environment.paths package

--- python_ci_toolkit/environment/paths/__init__.py
+++ python_ci_toolkit/environment/paths/__init__.py
@@ -1,9 +1,8 @@
-from .paths import (
-    ci_project_root,
-    ci_files_directory,
-    ci_files_directory_relative,
-    ci_temp_files_directory,
-    ci_temp_files_directory_relative,
-    ci_repo,
-    purge_temporary_files
-)
+from .core import CiPaths
+from .caches import purge_temporary_files
+
+ci_paths = CiPaths()
+
+__all__ = [
+    "ci_paths",
+]
