e0c1cd3748da6605ab2a69fbb3957121be9343cb
Yevhen Bondarenko <zekbond@gmail.com>
2024-01-25 15:38:27 +0200

    Update conftest

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,12 @@
 import pytest
 
 
+@pytest.hookimpl(hookwrapper=True)
+def pytest_pyfunc_call():
+    print()  # <- newline at the start of the logs to make them more readable
+    yield
+
+
 @pytest.fixture
 def variable_name() -> str:
     """
