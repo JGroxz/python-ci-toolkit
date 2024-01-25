2b2059f2f609fc86601f7bdf47844cfc7c7584fd
Yevhen Bondarenko <zekbond@gmail.com>
2024-01-25 15:37:12 +0200

    Rearrange the innards of environment.platform package

--- python_ci_toolkit/environment/platform/github.py
+++ python_ci_toolkit/environment/platform/github.py
@@ -5,6 +5,7 @@ from rich.console import Console
 from rich.theme import Theme
 
 from .base import CiPlatform
+from ...shell import run_shell_command
 
 
 class GitHubActions(CiPlatform):
@@ -26,7 +27,14 @@ class GitHubActions(CiPlatform):
 
     @classmethod
     def get_ci_project_root(cls) -> Path:
-        return Path(os.environ.get("GITHUB_WORKSPACE"))
+        ci_project_root = Path(os.getenv("GITHUB_WORKSPACE"))
+
+        # GitHub Actions workspace directory belongs to a different user out-of-the-box,
+        # so we have to mark it as safe to be able to run all git commands without errors.
+        # See for more info: https://github.com/python-semantic-release/python-semantic-release/issues/560
+        run_shell_command(f'git config --global --add safe.directory "{ci_project_root}"', silence_output=True, use_wsl_on_windows=False)
+
+        return ci_project_root
 
     @classmethod
     def on_patch_rich_console(cls, console: Console, default_theme: Theme) -> Console:
