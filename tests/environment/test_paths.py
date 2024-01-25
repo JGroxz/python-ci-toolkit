128b4e031ce1a7a645da7ccfd55504c7a14cc767
Yevhen Bondarenko <zekbond@gmail.com>
2024-01-25 20:54:46 +0200

    Fix typo in paths test

--- tests/environment/test_paths.py
+++ tests/environment/test_paths.py
@@ -30,4 +30,4 @@ def test_temp_directory_path():
     #
     # from python_ci_toolkit.environment import ci_project_root
     # assert ci_temp_files_directory.samefile(ci_project_root / ci_temp_files_directory_relative), \
-    #     "Relative temp CI directory path is does not point to the same directory as the absolute temp CI directory path."
+    #     "Relative temp CI directory path does not point to the same directory as the absolute temp CI directory path."
